6facc1485653bdab9bcae6c169535cd16b1794ed
Edimar Pereira <edpbatista2@gmail.com>
2022-09-16 11:41:21 -0400

    alteracao about

--- src/Section/About/index.jsx
+++ src/Section/About/index.jsx
@@ -1,5 +1,5 @@
 import './about.css'
-import { FaBriefcase, FaCalendarAlt, FaMapMarkerAlt , FaUniversity} from "react-icons/fa"
+import { FaBriefcase, FaCalendarAlt, FaMapMarkerAlt, FaUniversity } from "react-icons/fa"
 export default function About() {
 
   return (
@@ -9,86 +9,71 @@ export default function About() {
         <div className='experiencia'>
           <h4>Experiências Profissionais</h4>
           <br></br><br />
-          <div className='container'>
-            <div className='row flex'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
+          <div className='containerr'>
+            <div className='row flex coluna'>
+              <div className='col-6 colleft'>
                 <h6 className='title1'><FaBriefcase></FaBriefcase> Nucleotech Soluções Tecnologica</h6>
                 <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> Setembro-2021 / Atual</p>
                 <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
               </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
+              <div className="col-6 colright">
                 <h6 className='titlefuncao1'> Analista de Sistema de automação</h6>
                 <br /><br />
-                <p className='subtitlefun1'>- Atualização, estabilidade e disponibilidade de todos os recursos que
+                <p className='subtitlefun11'>- Atualização, estabilidade e disponibilidade de todos os recursos que
                   envolvem tecnologia, como software e hardware.</p>
                 <p className='subtitlefun1'>- Criação e administração de usuário no Active Directory.</p>
                 <p className='subtitlefun1'>- Criação e administração de e-mail exchange.</p>
                 <p className='subtitlefun1'>- Correção de problemas na infraestrutura, como configuração das
                   redes e servidores, recuperação de informações e estabelecimento de
-                  um plano de backup
-                  .</p>
+                  um plano de backup.</p>
               </div>
             </div>
-            <br></br><br></br>
-            <div className='row flex'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
+            <br /><br /><br />
+            <div className='row flex coluna'>
+              <div className='col-6 colleft'>
                 <h6 className='title1'><FaBriefcase></FaBriefcase> Atacadão S/A</h6>
                 <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> Maio-2020 / Setembro-2021</p>
                 <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
               </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
+              <div className="col-6 colright">
                 <h6 className='titlefuncao1'> Supervisor Administrativo</h6>
                 <br /><br />
-                <p className='subtitlefun1'>- Responsável pelo gerenciamento de pessoas, delegação de tarefas,
-                  avaliação de desempenho, mediação de conflitos, solução de crises,
-                  acompanhamento de resultados e, principalmente, desenvolvimento
+                <p className='subtitlefun11'>- Responsável pelo gerenciamento de pessoas.</p>
+                <p className='subtitlefun1'>- delegação de tarefas.</p>
+                <p className='subtitlefun1'>- avaliação de desempenho.</p>
+                <p className='subtitlefun1'>- mediação de conflitos e solução de crises.</p>
+                <p className='subtitlefun1'>- acompanhamento de resultados e principalmente, desenvolvimento
                   de competências da equipe.</p>
               </div>
             </div>
-            <br></br><br></br>
-            <div className='row'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
+            <br /><br /><br />
+            <div className='row flex coluna'>
+              <div className='col-6 colleft'>
                 <h6 className='title1'><FaBriefcase></FaBriefcase> Atacadão S/A</h6>
                 <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> Outubro-2018 / Maio-2020</p>
                 <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
               </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
+              <div className="col-6 colright">
                 <h6 className='titlefuncao1'> Auxiliar De Informática CPD</h6>
-                <br /><br /><br />
-                <p className='subtitlefun1'>- Alimentar equipamentos com suprimentos.</p>
+                <br /><br />
+                <p className='subtitlefun11'>- Alimentar equipamentos com suprimentos.</p>
                 <p className='subtitlefun1'>- Identificar falhas em hardware e software.</p>
-                <p className='subtitlefun1'>- Fazer cópias de segurança (backup).</p>
+                <p className='subtitlefun1'>- Realizar cópias de segurança (backup).</p>
                 <p className='subtitlefun1'>- Garantir segurança das informações.</p>
                 <p className='subtitlefun1'>- Administrar processamento de dados.</p>
               </div>
             </div>
-            <br></br><br></br>
-            <div className='row flex'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
+            <br /><br /><br />
+            <div className='row flex coluna'>
+              <div className='col-6 colleft'>
                 <h6 className='title1'><FaBriefcase></FaBriefcase> Atacadão S/A</h6>
-                <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> Março-2016 / Outubro-2018 </p>
-                <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
+                <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> Outubro-2018 / Maio-2020</p>
+                <p className='subtitle1'><FaMapMarkerAlt /> Cuiabá-MT, Brasil</p>
               </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
+              <div className="col-6 colright">
                 <h6 className='titlefuncao1'> Auxiliar de Almoxarifado</h6>
-                <br /><br /><br />
-                <p className='subtitlefun1'>- Coordenar entrada e saída de mercadorias.</p>
+                <br /><br />
+                <p className='subtitlefun11'>- Coordenar entrada e saída de mercadorias.</p>
                 <p className='subtitlefun1'>- Fazer registro e inventário de materiais permanentes ou de consumo.</p>
                 <p className='subtitlefun1'>- Conferir se os produtos recebidos estão de acordo com o que foi
                   solicitado, em quantidade e integridade.</p>
@@ -97,90 +82,67 @@ export default function About() {
               </div>
             </div>
           </div>
-
-
         </div>
         <br /><br /><br />
         <div className='experiencia'>
           <h4>Formação Educacional</h4>
-          <br></br>
-          <div className='container'>
-            <div className='row flex'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
-                <h6 className='title1'><FaUniversity></FaUniversity> Curso em Vídeo</h6>
-                <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> 2022 / 2022</p>
-                <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
-              </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
-                <h6 className='titlefuncao1'> Algoritmos, Java básico e Java POO</h6>
-              </div>
+          <br /><br /><br />
+          <div className='row flex coluna'>
+            <div className='col-6 colleft'>
+              <h6 className='title1'><FaUniversity></FaUniversity> Faculdade Invest de Ciências e Tecnologia</h6>
+              <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> Fevereiro-2021 / Julho-2023</p>
+              <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
             </div>
-            <br></br><br></br>
-            <div className='row flex'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
-                <h6 className='title1'><FaUniversity></FaUniversity> Faculdade Invest de Ciências e Tecnologia</h6>
-                <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> Fevereiro-2021 / Julho-2023</p>
-                <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
-              </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
-                <h6 className='titlefuncao1'> Análise e Desenvolvimento De Sistemas</h6>
-              </div>
-            </div>
-            <br></br><br></br>
-            <div className='row'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
-                <h6 className='title1'><FaUniversity></FaUniversity> Fundação Bradesco</h6>
-                <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> 2019 / 2019</p>
-                <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
-              </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
-                <h6 className='titlefuncao1'> Software e Hardware</h6>
-              </div>
-            </div>
-            <br></br><br></br>
-            <div className='row flex'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
-                <h6 className='title1'><FaUniversity></FaUniversity> Fundação Bradesco</h6>
-                <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> 2019 / 2019 </p>
-                <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
-              </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
-                <h6 className='titlefuncao1'> Segurança Da Informação</h6>
-              </div>
-            </div>
-            <br></br><br></br>
-            <div className='row flex'>
-              <div className='col-2'></div>
-              <div className="col-3 ">
-                <h6 className='title1'><FaUniversity></FaUniversity> Sebrae Mato Grosso</h6>
-                <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> 2020 / 2020 </p>
-                <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
-              </div>
-              <div className='col-2'>
-                <div class="linha-vertical"></div>
-              </div>
-              <div className="col-5">
-                <h6 className='titlefuncao1'> Liderança: como desenvolver times
-                  de alta performance</h6>
-              </div>
+            <div className="col-6 colright">
+              <h6 className='titlefuncao1'> Análise e Desenvolvimento De Sistemas</h6>
             </div>
           </div>
+          <br /><br /><br />
+          <div className='row flex coluna'>
+            <div className='col-6 colleft'>
+              <h6 className='title1'><FaUniversity></FaUniversity> Curso em Vídeo</h6>
+              <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> 2022 / 2022</p>
+              <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
+            </div>
+            <div className="col-6 colright">
+              <h6 className='titlefuncao1'> Algoritmos, Java básico e Java POO</h6>
+            </div>
+          </div>
+          <br /><br /><br />
+          <div className='row flex coluna'>
+            <div className='col-6 colleft'>
+              <h6 className='title1'><FaUniversity></FaUniversity> Sebrae Mato Grosso</h6>
+              <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> 2020 / 2020 </p>
+              <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
+            </div>
+            <div className="col-6 colright">
+              <h6 className='titlefuncao1'> Liderança: como desenvolver times
+                de alta performance</h6>
+            </div>
+          </div>
+          <br /><br /><br />
+          <div className='row flex coluna'>
+            <div className='col-6 colleft'>
+              <h6 className='title1'><FaUniversity></FaUniversity> Fundação Bradesco</h6>
+              <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> 2019 / 2019</p>
+              <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
+            </div>
+            <div className="col-6 colright">
+              <h6 className='titlefuncao1'> Software e Hardware</h6>
+            </div>
+          </div>
+          <br /><br /><br />
+          <div className='row flex coluna'>
+            <div className='col-6 colleft'>
+              <h6 className='title1'><FaUniversity></FaUniversity> Fundação Bradesco</h6>
+              <p className='subtitle1'><FaCalendarAlt></FaCalendarAlt> 2019 / 2019 </p>
+              <p className='subtitle1'><FaMapMarkerAlt></FaMapMarkerAlt> Cuiabá-MT, Brasil</p>
+            </div>
+            <div className="col-6 colright">
+              <h6 className='titlefuncao1'> Segurança Da Informação</h6>
+            </div>
+          </div>
+          <br /><br /><br />
           <div><br /><br /><br /></div>
         </div>
       </div>
--- src/Section/Footer/Footer.jsx
+++ src/Section/Footer/Footer.jsx
@@ -7,61 +7,61 @@ export default function Footer() {
 
     <div className='container-Footer row' style={{
       width: '100vw',
-      height: '50vh',
+      height: '30vh',
     }}>
       <div className='col-4' style={{
         color: '#E1E1E6',
-        fontSize: 25,
+        fontSize: 20,
         textAlign: 'center',
-        padding:70,
+        // padding:70,
       }}>
         <h1><FaMapMarkerAlt></FaMapMarkerAlt> Localização</h1>
         <div style={{
         color: '#E1E1E6',
-        fontSize: 15,
+        fontSize: 10,
         textAlign: 'center',
-        padding:15,
+        // padding:15,
       }}>
         <h2>Cuiaba-MT</h2>
         </div>
     </div>
     <div className='col-4' style={{
         color: '#E1E1E6',
-        fontSize: 25,
+        fontSize: 20,
         textAlign: 'center',
-        padding:70,
+        // padding:70,
       }}>
         <h1><FaMailBulk></FaMailBulk> Contato por E-mail</h1>
         <div style={{
         color: '#E1E1E6',
-        fontSize: 15,
+        fontSize: 10,
         textAlign: 'center',
-        padding:15,
+        // padding:15,
       }}>
         <h2>edpbatista2@gmail.com</h2>
         </div>
     </div>
     <div className='col-4' style={{
         color: '#E1E1E6',
-        fontSize: 25,
+        fontSize: 20,
         textAlign: 'center',
-        padding:70,
+        // padding:70,
       }}>
         <h1><FaPhoneSquareAlt></FaPhoneSquareAlt> Contato por Telefone</h1>
         <div style={{
         color: '#E1E1E6',
-        fontSize: 15,
+        fontSize: 10,
         textAlign: 'center',
-        padding:15,
+        // padding:15,
       }}>
         <h2>+55 (65) 99250-0235</h2>
         </div>
     </div>
     <div className='FooterFinal' style={{
         color: '#E1E1E6',
-        fontSize: 15,
+        fontSize: 10,
         textAlign: 'center',
-        padding:20,
+        // padding:20,
       }}>
       <h1>&copy;2022 - Edimar Pereira Batista</h1>
     </div>
